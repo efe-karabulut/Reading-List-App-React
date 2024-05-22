2108d5103b607412a0c245f31b9f12bcc28cabdc
efe-karabulut <efekarabulut1204@gmail.com>
2024-05-22 19:42:32 +0300

    Adding Json-server Api and changing CRUD Functions

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
-
-const randomNumber = 1000000;
-
-
+import axios from "axios";
 
 function App() {
   const [books, setBook] = useState([]);
 
-  const createBook = (bookTitle) => {
-    const updateBooks = [
-      ...books,
-      { id: Math.round(Math.random() * randomNumber), title: bookTitle },
-    ];
+  const fetchAllBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBook(response.data);
+  };
+
+  useEffect(() => {
+    fetchAllBooks();
+  }, []);
+
+  const createBook = async (bookTitle) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title: bookTitle,
+    });
+    const updateBooks = [...books, response.data];
     setBook(updateBooks);
   };
 
-  const deleteBookByID = (id) => {
+  const deleteBookByID = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBook(updatedBooks);
   };
 
-  const updateBookById = (id, newTitle) => {
+  const updateBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
-      } 
+        return { ...book, ...response.data };
+      }
       return book;
     });
     setBook(updatedBooks);
@@ -36,7 +47,7 @@ function App() {
 
   return (
     <div className="app">
-    <h1>Reading List</h1>
+      <h1>Reading List</h1>
       <BookCreate onCreate={createBook} />
       <BookList
         onEdit={updateBookById}
